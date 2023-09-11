6da4710ad9b3e150075338efae011938c9d57b61
lucio-souza <117987746+lucio-souza@users.noreply.github.com>
2023-09-11 18:24:02 -0300

    Update database list table in script.sql

--- scriptSQL/script.sql
+++ scriptSQL/script.sql
@@ -2,9 +2,10 @@ CREATE DATABASE
  BANKLIST;
  USE BANKLIST;
  CREATE TABLE banklist(
-    id int PRIMARY KEY,
-    due_date DATE,
-    total_in_cents varchar(50),
-    customer varchar(50),
-    status varchar(50)
+    id serial NOT NULL PRIMARY KEY,
+    due_date NOT NULL DATE,
+    payment_date DATE,
+    total_in_cents INT NOT NULL,
+    customer varchar(255) NOT NULL,
+    status varchar(30) NOT NULL
  );
