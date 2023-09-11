14b9b92e4aa13f398cb941f680fd51af1771b908
lucio-souza <117987746+lucio-souza@users.noreply.github.com>
2023-09-11 19:22:59 -0300

    Update script.sql populating the database

--- scriptSQL/script.sql
+++ scriptSQL/script.sql
@@ -1,11 +1,24 @@
+/*criação do banco de dados*/
 CREATE DATABASE
  BANKLIST;
  USE BANKLIST;
+
+/*criação da tabela banklist*/
  CREATE TABLE banklist(
     id serial NOT NULL PRIMARY KEY,
-    due_date NOT NULL DATE,
+    due_date DATE NOT NULL,
     payment_date DATE,
     total_in_cents INT NOT NULL,
     customer varchar(255) NOT NULL,
     status varchar(30) NOT NULL
  );
+
+ /*inserção de dados na tabela banklist*/
+ INSERT INTO banklist
+ VALUES
+(DEFAULT,'2023-09-14',NULL,'5000','andre silva','pending'),
+(DEFAULT,'2023-05-29',NULL,'20000','joao barros','pending'),
+(DEFAULT,'2023-07-20',NULL,'20000','maria souza','pending'),
+(DEFAULT,'2023-10-02',NULL,'10000','anderson santos','pending'),
+(DEFAULT,'2023-07-28',NULL,'2000','bruno filho','pending');
+
